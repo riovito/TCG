9645332f80e65bd5660bd4cd7e48f2990cf6dd3f
VIto <200763@upf.br>
2024-11-26 15:31:59 -0300

    edit.blade
    Adição da função para edição de raridades presentes no banco de dados

--- resources/views/raridades/edit.blade.php
+++ resources/views/raridades/edit.blade.php
@@ -1,42 +1,42 @@
 <x-app-layout>
-    <h1 class="text-white">Tipo EDIT</h1>
+    <h1 class="text-white">Raridade EDIT</h1>
 
     <section class="container mt-4">
         <div class="row justify-content-center">
             <div class="col-md-6">
-                <form method="POST" action="{{ route('tipos.update', $tipo->id) }}">
+                <form method="POST" action="{{ route('raridades.update', $raridade->id) }}">
                     @csrf
                     @method('PUT')
 
                     <div class="mb-3">
                         <label class="form-label text-light" for="nome">Nome:</label>
-                        <input type="text" id="nome" name="nome" value="{{ old('nome', $tipo->nome) }}" class="form-control">
+                        <input type="text" id="nome" name="nome" value="{{ old('nome', $raridade->nome) }}" class="form-control">
                     </div>
                     
 
                     <div class="mb-3">
-                        <label class="form-label text-light" for="fraqueza">Fraqueza:</label>
-                        <input type="number" id="fraqueza" name="fraqueza" value="{{ old('fraqueza', $tipo->fraqueza) }}" class="form-control">
+                        <label class="form-label text-light" for="chance">Chance:</label>
+                        <input type="number" id="chance" name="chance" value="{{ old('chance', $raridade->chance) }}" class="form-control">
                     </div>
 
                     <div class="mb-3">
-                        <label class="form-label text-light" for="resistencia">Resistencia:</label>
-                        <input type="number" id="resistencia" name="resistencia" value="{{ old('resistencia', $tipo->resistencia) }}" class="form-control">
+                        <label class="form-label text-light" for="descricao">Descricao:</label>
+                        <input type="number" id="descricao" name="descricao" value="{{ old('descricao', $raridade->descricao) }}" class="form-control">
                     </div>
 
                     <div class="mb-3">
-                        <label class="form-label text-light" for="cor">Cor:</label>
-                        <input type="text" id="cor" name="cor" value="{{ old('cor', $tipo->cor) }}" class="form-control">
+                        <label class="form-label text-light" for="exclusividade">Exclusividade:</label>
+                        <input type="text" id="exclusividade" name="exclusividade" value="{{ old('exclusividade', $raridade->exclusividade) }}" class="form-control">
                     </div>
 
                     <div class="mb-3">
-                        <label class="form-label text-light" for="efetivo">Efetivo:</label>
-                        <input type="text" id="efetivo" name="efetivo" value="{{ old('efetivo', $tipo->efetivo) }}" class="form-control">
+                        <label class="form-label text-light" for="estrelas">Estrelas:</label>
+                        <input type="text" id="estrelas" name="estrelas" value="{{ old('estrelas', $raridade->estrelas) }}" class="form-control">
                     </div>
 
                     <div class="mb-3 d-flex justify-content-between">
                         <button type="submit" class="btn btn-primary">Salvar</button>
-                        <a href="{{ route('tipos.index') }}" class="btn btn-secondary">Cancelar</a>
+                        <a href="{{ route('raridades.index') }}" class="btn btn-secondary">Cancelar</a>
                     </div>
                 </form> 
             </div>
