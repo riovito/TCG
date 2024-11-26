7fb3f38ad03f7642c3756b485f316e10a83dc74e
VIto <200763@upf.br>
2024-11-26 15:27:31 -0300

    index.blade
    Index agora mostrando as raridades presentes no banco de dados. Navigation bar mostrando opção para Raridades

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -23,6 +23,12 @@
                     </x-nav-link>
                 </div>
 
+                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                    <x-nav-link  href="{{ route('raridades.index') }}">
+                        {{ __('Raridades') }}
+                    </x-nav-link>
+                </div>
+
                 
             </div>
 
