91d59f350c9efbd8d61c0f2806d0df55c0edee98
VIto <200763@upf.br>
2024-11-26 14:46:44 -0300

    Update index.blade
    Update do arquivo index.blade

--- resources/views/tipos/index.blade.php
+++ resources/views/tipos/index.blade.php
@@ -6,12 +6,10 @@
             <tr>
                 <th>ID</th>
                 <th>Nome</th>
-                <th>Ataque1</th>
-                <th>Ataque2</th>
-                <th>Hp</th>
-                <th>Preco</th>
-                <th>Tipo</th>
-                <th>Raridade</th>
+                <th>Fraqueza</th>
+                <th>Resistencia</th>
+                <th>Cor</th>
+                <th>Efetivo</th>
                 <th></th>
             </tr>
 
@@ -19,30 +17,19 @@
         
 
         <tbody>
-            @foreach ($cartas as $carta)
+            @foreach ($tipos as $tipo)
                 <tr>
-                    <td class="colunas">{{ $carta->id }}</td>
-                    <td id="nome">{{ $carta->nome }}</td>
-                    <td id="ataque1">{{ $carta->ataque1 }}</td>
-                    <td id="ataque2">{{ $carta->ataque2 }}</td>
-                    <td id="hp">{{ $carta->hp }}</td>
-                    <td id="preco">{{ $carta->preco }}</td>
-                    <td id="tipo">{{ $carta->tipo }}</td>
-                    <td id="raridade">{{ $carta->raridade }}</td>
+                    <td class="colunas">{{ $tipo->id }}</td>
+                    <td id="nome">{{ $tipo->nome }}</td>
+                    <td id="fraqueza">{{ $tipo->fraqueza }}</td>
+                    <td id="resistencia">{{ $tipo->resistencia }}</td>
+                    <td id="cor">{{ $tipo->cor }}</td>
+                    <td id="efetivo">{{ $tipo->efetivo }}</td>
                     
-                    <td>
-                         <a href="{{ route('carta.show', $carta->id) }}" class="btn btn-info">Detalhes</a>
-                        <a href="{{ route('carta.edit', $carta->id) }}" class="btn btn-warning">Editar</a>
-                        <form action="{{ route('carta.destroy', $carta->id) }}" method="POST" style="display: inline;">
-                            @csrf
-                            @method('DELETE')
-                            <button type="submit" class="btn btn-danger">Excluir</button>
-                        </form>
-                    </td>
                 </tr>
             @endforeach
         </tbody>
     </table>
 
-    <a href="cartas/create"><button type="button" class="btn btn-outline-secondary btn-lg px-4 gap-3 text-white">CREATE</button></a>
+    <a href="tipos/create"><button type="button" class="btn btn-outline-secondary btn-lg px-4 gap-3 text-white">CREATE</button></a>
 </x-app-layout>
