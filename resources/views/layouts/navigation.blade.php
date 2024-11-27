dc3e10302f8d11025b751835240b1a453c2d1ab9
VIto <200763@upf.br>
2024-11-27 10:56:08 -0300

    tabela users
    Personalizada tabela users

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,10 +1,10 @@
 <nav x-data="{ open: false }" class="hover">
     <!-- Primary Navigation Menu -->
-    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+    <div class="hover bg-gray-800 w-full text-white">
         <div class="flex justify-between h-16">
             <div class="flex">
                 <!-- Logo -->
-                <div class="shrink-0 flex items-center">
+                <div class="shrink-0 flex items-center ml-3">
                     <a href="{{ route('dashboard') }}">
                         <img class="d-block" style="width: 40px;" src="{{ asset('img/icon-cut.png') }}" alt="Icone">
                     </a>
@@ -45,7 +45,7 @@
             </div>
 
             <!-- Settings Dropdown -->
-            <div class="hidden sm:flex sm:items-center sm:ms-6">
+            <div class="hidden sm:flex sm:items-center sm:ms-6 m-3">
                 <x-dropdown align="right" width="48">
                     <x-slot name="trigger">
                         <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150">
